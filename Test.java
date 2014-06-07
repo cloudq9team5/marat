64ea5460a4dbb7368200803459158067633073b9
marat <cloudq9team5@gmail.com>
2014-06-07 22:59:40 +0900

    upload

---
+++ Test.java
@@ -0,0 +1,21 @@
+package test;
+import java.io.*;
+import java.util.Date;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+public class Test {
+	public static void main (String[] args) { try {
+		MongoClient mongo = new MongoClient( "localhost", 12345);
+		DB db = mongo.getDB( "tem"); // mongo > db
+		DBCollection table = db.getCollection( "user"); // db > table
+		BasicDBObject query = new BasicDBObject() // query
+		.append( "name", "marat");	// search for key=value
+		DBCursor cursor = table.find( query);
+		if ( cursor.hasNext()) System.out.println( ( String)cursor.next()); // only first result
+	} catch ( MongoException e) { System.out.println( "{ \"error\": \"bad code\"}");}}
+	
+}
