ca4c5598ccc2ac013b253f36936f3edc375bc2d8
marat <cloudq9team5@gmail.com>
2014-06-07 21:47:13 +0900

    upload

--- src/java/test/Test.java
+++ src/java/test/Test.java
@@ -2,7 +2,7 @@ package test;
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.*;
-public class HelloServlet extends HttpServlet {
+public class Test extends HttpServlet {
 	public void doPost (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		PrintWriter out = res.getWriter();
 		out.println("Hello, world!");
