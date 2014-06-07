43285df8c23751dc5a9b15dbf28e9cdaec99cda0
marat <cloudq9team5@gmail.com>
2014-06-08 07:25:44 +0900

    upload

--- actions.php
+++ actions.php
@@ -1,5 +1,6 @@
 <?php
 $m = new MongoClient( 'mongodb://localhost:12345');
-$info = $m->tem->users->find( array( 'name' => 'marat'));
-echo json_encode( $info); 
+$cursor = $m->tem->users->find( array( 'name' => 'marat'));
+$result = $cursor->getNext();
+echo json_encode( $result); 
 ?>
