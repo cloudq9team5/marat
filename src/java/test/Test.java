52e0506d23beb922702cfa1c39fcc6d5c7ab716c
marat <cloudq9team5@gmail.com>
2014-06-07 23:12:53 +0900

    upload

--- src/java/test/Test.java
+++ src/java/test/Test.java
@@ -11,15 +11,19 @@ import com.mongodb.MongoException;
 import javax.servlet.http.*;
 import javax.servlet.*;
 public class Test extends HttpServlet {
-	public void doPost( HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException { try { 
-		PrintWriter out = res.getWriter(); // HTML output
-		MongoClient mongo = new MongoClient("localhost", 12345);
-		DB db = mongo.getDB( "tem"); // mongo > db
-		DBCollection table = db.getCollection( "user"); // db > table
-		BasicDBObject query = new BasicDBObject() // query
-		.append( "name", "marat");	// search for key=value
-		DBCursor cursor = table.find( query);
-		if ( cursor.hasNext()) out.println( ( String)cursor.next()); // only first result
-	} catch ( MongoException e) { res.getWriter().println( "{ \"error\": \"bad code\"}");}}
+	public void doPost( HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException { 
+		try { 
+			PrintWriter out = res.getWriter(); // HTML output
+			MongoClient mongo = new MongoClient("localhost", 12345);
+			DB db = mongo.getDB( "tem"); // mongo > db
+			DBCollection table = db.getCollection( "user"); // db > table
+			BasicDBObject query = new BasicDBObject() // query
+			.append( "name", "marat");	// search for key=value
+			DBCursor cursor = table.find( query);
+			if ( cursor.hasNext()) out.println( cursor.next()); // only first result
+		} 
+		catch ( MongoException e) { res.getWriter().println( "{ \"error\": \"bad code\"}"); }
+		catch ( UnknownHostException e) { System.out.println( "{ \"error\": \"bad code\"}");}
+	}
 	
 }
