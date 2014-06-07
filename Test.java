b0b6692a0de17e8dba85f0bdb186f6dbe9879340
marat <cloudq9team5@gmail.com>
2014-06-07 23:08:06 +0900

    upload

--- Test.java
+++ Test.java
@@ -8,14 +8,18 @@ import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 public class Test {
-	public static void main (String[] args) { try {
-		MongoClient mongo = new MongoClient( "localhost", 12345);
-		DB db = mongo.getDB( "tem"); // mongo > db
-		DBCollection table = db.getCollection( "user"); // db > table
-		BasicDBObject query = new BasicDBObject() // query
-		.append( "name", "marat");	// search for key=value
-		DBCursor cursor = table.find( query);
-		if ( cursor.hasNext()) System.out.println( cursor.next().toString()); // only first result
-	} catch ( MongoException e) { System.out.println( "{ \"error\": \"bad code\"}");}}
-	
+	public static void main (String[] args) { 
+		try {
+			MongoClient mongo = new MongoClient( "localhost", 12345);
+			DB db = mongo.getDB( "tem"); // mongo > db
+			DBCollection table = db.getCollection( "user"); // db > table
+			BasicDBObject query = new BasicDBObject() // query
+			.append( "name", "marat");	// search for key=value
+			DBCursor cursor = table.find( query);
+			if ( cursor.hasNext()) System.out.println( cursor.next().toString()); // only first result
+		} 
+		catch ( MongoException e) { System.out.println( "{ \"error\": \"bad code\"}")}
+		catch ( UnknownHostException e) { System.out.println( "{ \"error\": \"bad code\"}")}
+	}
+		
 }
