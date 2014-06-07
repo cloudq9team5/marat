5b4cd69a55523d1d4fbf0823b2fe7ec2de61c4c5
marat <cloudq9team5@gmail.com>
2014-06-07 22:23:09 +0900

    upload

--- src/java/test/Test.java
+++ src/java/test/Test.java
@@ -3,7 +3,7 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.*;
 public class Test extends HttpServlet {
-	public void doPost (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	public void doPost( HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		PrintWriter out = res.getWriter();
 		out.println( "{ \"name\": \"marat\", \"age\": 37}");
 		out.close();
