15a7f73d3dc1c6449cd8fc846ba0b0ef09f12791
marat <cloudq9team5@gmail.com>
2014-06-07 22:49:04 +0900

    upload

--- src/java/test/Test.java
+++ src/java/test/Test.java
@@ -1,12 +1,25 @@
 package test;
 import java.io.*;
+import java.net.UnknownHostException;
+import java.util.Date;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
 import javax.servlet.http.*;
 import javax.servlet.*;
 public class Test extends HttpServlet {
-	public void doPost( HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		PrintWriter out = res.getWriter();
-		out.println( "{ \"name\": \"marat\", \"age\": 37}");
-		out.close();
-	}
+	public void doPost( HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException { try { 
+		PrintWriter out = res.getWriter(); // HTML output
+		MongoClient mongo = new MongoClient("localhost", 12345);
+		DB db = mongo.getDB( 'tem'); // mongo > db
+		DBCollection table = db.getCollection( "user"); // db > table
+		BasicDBObject query = new BasicDBObject() // query
+		.append( "name", "marat");	// search for key=value
+		DBCursor cursor = table.find( query);
+		if ( cursor.hasNext()) out.println( cursor.next()); // only first result
+	} catch ( MongoException e) { out.println( "{ \"error\": \"bad code\"}");}}
 	
 }
